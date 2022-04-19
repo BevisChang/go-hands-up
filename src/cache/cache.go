d75bfe43720ce7933c53340465857864b1fdd793
Bevis Chang <bevis.chang@amazingtalker.com>
2022-04-19 11:36:51 +0800

    feat: add comment

--- src/cache/cache.go
+++ src/cache/cache.go
@@ -1,7 +1,9 @@
 package localcache
 
 type Cache interface {
+	// Get cache by key
 	Get(string) (interface{}, error)
 
+	// Set cache via provides key and value, you can provide any type of value
 	Set(string, interface{}) error
 }
